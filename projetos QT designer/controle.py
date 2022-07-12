b7f52abf966c1b0d89646b8a3107b2bd61606429
Gabriel Correa de Souza <83256077+tcastgabriel@users.noreply.github.com>
2022-07-12 00:26:41 -0300

    Add files via upload

--- projetos QT designer/controle.py
+++ projetos QT designer/controle.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic,QtWidgets
 import mysql.connector
+from reportlab.pdfgen import canvas
 
 banco = mysql.connector.connect(
     host="localhost",
@@ -7,6 +8,44 @@ banco = mysql.connector.connect(
     passwd="",
     database="cadastro_produtos"
 )
+
+def excluir_dados():
+    linha = segunda_tela.tableWidget.currentRow()
+    segunda_tela.tableWidget.removeRow(linha)
+
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+    cursor.execute("DELETE FROM produtos WHERE id="+ str(valor_id))
+
+def gerar_pdf():
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+    y = 0
+    pdf = canvas.Canvas("cadastro_produtos.pdf")
+    pdf.setFont("Times-Bold", 25)
+    pdf.drawString(200,800, "Produtos cadastrados:")
+    pdf.setFont("Times-Bold", 18)
+
+    pdf.drawString(10,750, "ID")
+    pdf.drawString(110,750, "CODIGO")
+    pdf.drawString(210,750, "PRODUTO")
+    pdf.drawString(310,750, "PREÇO")
+    pdf.drawString(410,750, "CATEGORIA")
+
+    for i in range(0, len(dados_lidos)):
+        y = y + 50
+        pdf.drawString(10,750 - y, str(dados_lidos[i][0]))
+        pdf.drawString(110,750 - y, str(dados_lidos[i][1]))
+        pdf.drawString(210,750 - y, str(dados_lidos[i][2]))
+        pdf.drawString(310,750 - y, str(dados_lidos[i][3]))
+        pdf.drawString(410,750 - y, str(dados_lidos[i][4]))
+
+    pdf.save()
+    print("PDF FOI GERADO COM SUCESSO!")
 
 def funcao_principal():
     linha1 = formulario.lineEdit.text()
@@ -17,10 +56,10 @@ def funcao_principal():
 
     if formulario.radioButton.isChecked() :
         print("Categoria Informatica foi selecionada")
-        categoria ="Eletronicos"
+        categoria ="Informatica"
     elif formulario.radioButton_2.isChecked() :
         print("Categoria eletronicos foi selecionada")
-        categoria ="Informatica"     
+        categoria ="Eletronicos"
     
     print("Codigo:" ,linha1)
     print("Descricao:" ,linha2)
@@ -31,20 +70,38 @@ def funcao_principal():
     dados = (str(linha1),str(linha2),str(linha3),categoria)
     cursor.execute(comando_SQL,dados)
     banco.commit()
+    formulario.lineEdit.setText("")
+    formulario.lineEdit_2.setText("")
+    formulario.lineEdit_3.setText("")
 
+def chama_segunda_tela():
+    segunda_tela.show()
+
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+   
+    segunda_tela.tableWidget.setRowCount(len(dados_lidos))
+    segunda_tela.tableWidget.setColumnCount(5)
+
+    for i in range(0, len(dados_lidos)):
+        for j in range(0, 5):
+            segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("formulario.ui")
+segunda_tela=uic.loadUi("listar_dados.ui")
 formulario.pushButton.clicked.connect(funcao_principal)
+formulario.pushButton_2.clicked.connect(chama_segunda_tela)
+segunda_tela.pushButton.clicked.connect(gerar_pdf)
+segunda_tela.pushButton_2.clicked.connect(excluir_dados)
+
+
 
 formulario.show()
 app.exec()
-
-
-
-
-
 
 
 
